3202b4a1a5e34a8a03f2eb2b32547e4c4f38acd3
JamesVanWaza <jgacuca@gmail.com>
2020-07-14 18:53:20 -0400

    Changed the version of Firebase

--- web-start/public/firebase-messaging-sw.js
+++ web-start/public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('/__/firebase/7.14.5/firebase-app.js');
-importScripts('/__/firebase/7.14.5/firebase-messaging.js');
+importScripts('/__/firebase/7.16.0/firebase-app.js');
+importScripts('/__/firebase/7.16.0/firebase-messaging.js');
 importScripts('/__/firebase/init.js');
 
 firebase.messaging();
