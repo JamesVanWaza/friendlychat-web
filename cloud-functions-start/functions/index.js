85210ade1bf556994caaed1ad5d5194f801f8247
JamesVanWaza <jgacuca@gmail.com>
2020-07-15 11:15:00 -0400

    WIP, awaiting support to see if they can resolve issue.

--- cloud-functions-start/functions/index.js
+++ cloud-functions-start/functions/index.js
@@ -26,14 +26,14 @@ exports.addWelcomeMessages = functions.auth.user().onCreate(async(user) => {
     console.log('A new user signed in for the first time.');
     const fullName = user.displayName || 'Anonymous';
 
-    // Saves the new welcome message into the database which then displays it in the FriendlyChat clients
+    // Saves the new welcome message into the database which then displays it in the FriendlyChat clients.
     await admin.firestore().collection('messages').add({
         name: 'Firebase Bot',
-        profilePicUrl: '/images/firebase-logo.png', // Firebase Logo
+        profilePicUrl: '/images/firebase-logo.png', // Firebase logo
         text: `${fullName} signed in for the first time! Welcome!`,
         timestamp: admin.firestore.FieldValue.serverTimestamp()
     });
-    console.log('Welcome message written to database');
+    console.log('Welcome message written to database.');
 });
 
 // TODO(DEVELOPER): Write the blurOffensiveImages Function here.
